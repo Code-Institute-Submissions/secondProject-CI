0166bfe6dfa494455cf15e3bf7859d5fbcbdaa6a
Xasthar <luksor1_1991@o2.pl>
2020-05-11 21:43:00 +0000

    Deleted unused imges, changed map api to mapbox, added listing and js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,10 +1,401 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGl0a28iLCJhIjoiY2thMWhidHUwMDBoNzNtbWxrNXpyNXZ0YSJ9.eSoYXrssVsLiOkLLoZNl1w';
+var map = new mapboxgl.Map({
+    container: 'map',
+    center: [19.627, 52.030],
+    zoom: '5.90',
+    style: 'mapbox://styles/mapbox/streets-v11'
+});
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiZGl0a28iLCJhIjoiY2thMWhidHUwMDBoNzNtbWxrNXpyNXZ0YSJ9.eSoYXrssVsLiOkLLoZNl1w'
-}).addTo(mymap);
+var spots = {
+        "type": "FeatureCollection",
+        "features": [
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+                16.241986,
+                50.451568
+              ]
+            },
+            "properties": {
+              "name": "Skull Chapel",
+              "address": "Stanisława Moniuszki 8a",
+              "city": "Kudowa-Zdrój",
+              "country": "Poland",
+              "postalCode": "57-350",
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+                -77.049766,
+                38.900772
+              ]
+            },
+            "properties": {
+              "phoneFormatted": "(202) 507-8357",
+              "phone": "2025078357",
+              "address": "2221 I St NW",
+              "city": "Washington DC",
+              "country": "United States",
+              "crossStreet": "at 22nd St NW",
+              "postalCode": "20037",
+              "state": "D.C."
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+                -77.043929,
+                38.910525
+              ]
+            },
+            "properties": {
+              "phoneFormatted": "(202) 387-9338",
+              "phone": "2023879338",
+              "address": "1512 Connecticut Ave NW",
+              "city": "Washington DC",
+              "country": "United States",
+              "crossStreet": "at Dupont Circle",
+              "postalCode": "20036",
+              "state": "D.C."
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+                -77.0672,
+                38.90516896
+              ]
+            },
+            "properties": {
+              "phoneFormatted": "(202) 337-9338",
+              "phone": "2023379338",
+              "address": "3333 M St NW",
+              "city": "Washington DC",
+              "country": "United States",
+              "crossStreet": "at 34th St NW",
+              "postalCode": "20007",
+              "state": "D.C."
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+                -77.002583742142,
+                38.887041080933
+              ]
+            },
+            "properties": {
+              "phoneFormatted": "(202) 547-9338",
+              "phone": "2025479338",
+              "address": "221 Pennsylvania Ave SE",
+              "city": "Washington DC",
+              "country": "United States",
+              "crossStreet": "btwn 2nd & 3rd Sts. SE",
+              "postalCode": "20003",
+              "state": "D.C."
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+                -76.933492720127,
+                38.99225245786
+              ]
+            },
+            "properties": {
+              "address": "8204 Baltimore Ave",
+              "city": "College Park",
+              "country": "United States",
+              "postalCode": "20740",
+              "state": "MD"
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+                -77.097083330154,
+                38.980979
+              ]
+            },
+            "properties": {
+              "phoneFormatted": "(301) 654-7336",
+              "phone": "3016547336",
+              "address": "4831 Bethesda Ave",
+              "cc": "US",
+              "city": "Bethesda",
+              "country": "United States",
+              "postalCode": "20814",
+              "state": "MD"
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+                -77.359425054188,
+                38.958058116661
+              ]
+            },
+            "properties": {
+              "phoneFormatted": "(571) 203-0082",
+              "phone": "5712030082",
+              "address": "11935 Democracy Dr",
+              "city": "Reston",
+              "country": "United States",
+              "crossStreet": "btw Explorer & Library",
+              "postalCode": "20190",
+              "state": "VA"
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+                -77.10853099823,
+                38.880100922392
+              ]
+            },
+            "properties": {
+              "phoneFormatted": "(703) 522-2016",
+              "phone": "7035222016",
+              "address": "4075 Wilson Blvd",
+              "city": "Arlington",
+              "country": "United States",
+              "crossStreet": "at N Randolph St.",
+              "postalCode": "22203",
+              "state": "VA"
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+                -75.28784,
+                40.008008
+              ]
+            },
+            "properties": {
+              "phoneFormatted": "(610) 642-9400",
+              "phone": "6106429400",
+              "address": "68 Coulter Ave",
+              "city": "Ardmore",
+              "country": "United States",
+              "postalCode": "19003",
+              "state": "PA"
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+                -75.20121216774,
+                39.954030175164
+              ]
+            },
+            "properties": {
+              "phoneFormatted": "(215) 386-1365",
+              "phone": "2153861365",
+              "address": "3925 Walnut St",
+              "city": "Philadelphia",
+              "country": "United States",
+              "postalCode": "19104",
+              "state": "PA"
+            }
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+                -77.043959498405,
+                38.903883387232
+              ]
+            },
+            "properties": {
+              "phoneFormatted": "(202) 331-3355",
+              "phone": "2023313355",
+              "address": "1901 L St. NW",
+              "city": "Washington DC",
+              "country": "United States",
+              "crossStreet": "at 19th St",
+              "postalCode": "20036",
+              "state": "D.C."
+            }
+          }
+        ]
+      };
+      
+      /**
+       * Assign a unique id to each store. You'll use this `id`
+       * later to associate each point on the map with a listing
+       * in the sidebar.
+      */
+      spots.features.forEach(function(spot, i){
+        spot.properties.id = i;
+      });
+
+      /**
+       * Wait until the map loads to make changes to the map.
+      */
+      map.on('load', function (e) {
+        /** 
+         * This is where your '.addLayer()' used to be, instead
+         * add only the source without styling a layer
+        */
+        map.addSource("places", {
+          "type": "geojson",
+          "data": spots
+        });
+
+        /**
+         * Add all the things to the page:
+         * - The location listings on the side of the page
+         * - The markers onto the map
+        */
+        buildLocationList(spots);
+        addMarkers();
+      });
+
+      /**
+       * Add a marker to the map for every store listing.
+      **/
+      function addMarkers() {
+        /* For each feature in the GeoJSON object above: */
+        spots.features.forEach(function(marker) {
+          /* Create a div element for the marker. */
+          var el = document.createElement('div');
+          /* Assign a unique `id` to the marker. */
+          el.id = "marker-" + marker.properties.id;
+          /* Assign the `marker` class to each marker for styling. */
+          el.className = 'marker';
+          
+          /**
+           * Create a marker using the div element
+           * defined above and add it to the map.
+          **/
+          new mapboxgl.Marker(el, { offset: [0, -23] })
+            .setLngLat(marker.geometry.coordinates)
+            .addTo(map);
+
+          /**
+           * Listen to the element and when it is clicked, do three things:
+           * 1. Fly to the point
+           * 2. Close all other popups and display popup for clicked store
+           * 3. Highlight listing in sidebar (and remove highlight for all other listings)
+          **/
+          el.addEventListener('click', function(e){
+            /* Fly to the point */
+            flyToSpot(marker);
+            /* Close all other popups and display popup for clicked store */
+            createPopUp(marker);
+            /* Highlight listing in sidebar */
+            var activeItem = document.getElementsByClassName('active');
+            e.stopPropagation();
+            if (activeItem[0]) {
+              activeItem[0].classList.remove('active');
+            }
+            var listing = document.getElementById('listing-' + marker.properties.id);
+            listing.classList.add('active');
+          });
+        });
+      }
+
+      /**
+       * Add a listing for each store to the sidebar.
+      **/
+      function buildLocationList(data) {
+        data.features.forEach(function(spot, i){
+          /**
+           * Create a shortcut for `store.properties`,
+           * which will be used several times below.
+          **/
+          var prop = spot.properties;
+
+          /* Add a new listing section to the sidebar. */
+          var listings = document.getElementById('listings');
+          var listing = listings.appendChild(document.createElement('div'));
+          /* Assign a unique `id` to the listing. */
+          listing.id = "listing-" + prop.id;
+          /* Assign the `item` class to each listing for styling. */
+          listing.className = 'item';
+
+          /* Add the link to the individual listing created above. */
+          var link = listing.appendChild(document.createElement('a'));
+          link.href = '#';
+          link.className = 'title';
+          link.id = "link-" + prop.id;
+          link.innerHTML = prop.name;
+
+          /* Add details to the individual listing. */
+          var details = listing.appendChild(document.createElement('div'));
+          details.innerHTML = prop.city;
+          if (prop.address) {
+            details.innerHTML += ' · ' + prop.address;
+          }
+
+          /**
+           * Listen to the element and when it is clicked, do four things:
+           * 1. Update the `currentFeature` to the store associated with the clicked link
+           * 2. Fly to the point
+           * 3. Close all other popups and display popup for clicked store
+           * 4. Highlight listing in sidebar (and remove highlight for all other listings)
+          **/
+          link.addEventListener('click', function(e){
+            for (var i=0; i < data.features.length; i++) {
+              if (this.id === "link-" + data.features[i].properties.id) {
+                var clickedListing = data.features[i];
+                flyToSpot(clickedListing);
+                createPopUp(clickedListing);
+              }
+            }
+            var activeItem = document.getElementsByClassName('active');
+            if (activeItem[0]) {
+              activeItem[0].classList.remove('active');
+            }
+            this.parentNode.classList.add('active');
+          });
+        });
+      }
+
+      /**
+       * Use Mapbox GL JS's `flyTo` to move the camera smoothly
+       * a given center point.
+      **/
+      function flyToSpot(currentFeature) {
+        map.flyTo({
+          center: currentFeature.geometry.coordinates,
+          zoom: 15
+        });
+      }
+
+      /**
+       * Create a Mapbox GL JS `Popup`.
+      **/
+      function createPopUp(currentFeature) {
+        var popUps = document.getElementsByClassName('mapboxgl-popup');
+        if (popUps[0]) popUps[0].remove();
+        var popup = new mapboxgl.Popup({closeOnClick: true})
+          .setLngLat(currentFeature.geometry.coordinates)
+          .setHTML('<h3>' + currentFeature.properties.name + '</h3>' +
+            '<h4>' + currentFeature.properties.address + '</h4>')
+          .addTo(map);
+      }
